a4ab8bd06e97db109e03b76c80668b21f78f8907
FoXiKk101 <57393741+FoXiKk101@users.noreply.github.com>
2024-06-28 17:20:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let score = 0;
+    let score = localStorage.getItem('score') ? parseInt(localStorage.getItem('score')) : 0;
     const scoreElement = document.getElementById('score');
     const clickerButton = document.getElementById('clickerButton');
     const coinImage = document.getElementById('coinImage');
 
+    // Обновляем отображение счета при загрузке страницы
+    scoreElement.textContent = score.toLocaleString();
+
     clickerButton.addEventListener('click', () => {
         score++;
         scoreElement.textContent = score.toLocaleString();
+        localStorage.setItem('score', score);
 
         // Добавить класс анимации при клике
         coinImage.classList.add('clicked');
