2ad063d5d062380320e3e14b7e1596d3c038a0bc
FoXiKk101 <57393741+FoXiKk101@users.noreply.github.com>
2024-06-28 17:15:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     let score = 0;
     const scoreElement = document.getElementById('score');
     const clickerButton = document.getElementById('clickerButton');
+    const coinImage = document.getElementById('coinImage');
 
     clickerButton.addEventListener('click', () => {
         score++;
         scoreElement.textContent = score.toLocaleString();
+
+        // Добавить класс анимации при клике
+        coinImage.classList.add('clicked');
+
+        // Удалить класс анимации после завершения анимации
+        setTimeout(() => {
+            coinImage.classList.remove('clicked');
+        }, 100);
     });
 });
